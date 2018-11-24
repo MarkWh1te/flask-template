345a5b7997e0b7e457b0bda8ab3b9b78375a977a
ltaniguti <lucas.biotec07@gmail.com>
2018-11-24 16:20:33 -0200

    coverage print

--- escola.py
+++ escola.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import coverage
 from app import create_app
 COV = coverage.coverage(branch=True, include='app/*')
@@ -8,10 +9,12 @@ app = create_app("dev")
 @app.cli.command()
 def test():
     """Run the unit tests."""
+    basedir = os.path.abspath(os.path.dirname(__file__))
     COV.start()
     import unittest
     tests = unittest.TestLoader().discover('tests')
     exit_code = unittest.TextTestRunner().run(tests).wasSuccessful()
     COV.stop()
     COV.save()
+    COV.report()
     sys.exit(0 if exit_code else 1)
