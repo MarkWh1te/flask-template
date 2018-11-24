d6461f56a4456f4084242534834c466aaf80167d
ltaniguti <lucas.biotec07@gmail.com>
2018-11-24 15:36:34 -0200

    code coverage

--- escola.py
+++ escola.py
@@ -1,13 +1,17 @@
 import sys
+import coverage
 from app import create_app
+COV = coverage.coverage(branch=True, include='app/*')
 
 app = create_app("dev")
 
 @app.cli.command()
-# @click.option('--coverage/--no-coverage', default=False, help='Run tests under code coverage.')
 def test():
     """Run the unit tests."""
+    COV.start()
     import unittest
     tests = unittest.TestLoader().discover('tests')
     exit_code = unittest.TextTestRunner().run(tests).wasSuccessful()
+    COV.stop()
+    COV.save()
     sys.exit(0 if exit_code else 1)
